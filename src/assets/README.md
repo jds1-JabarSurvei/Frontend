3550efb848d5260959dc02bea45f5a104ed829ef
Ananda Yulizar <anandayulizar@gmail.com>
2021-02-09 14:41:55 +0700

    Created simple folder structure

---
+++ src/assets/README.md
@@ -0,0 +1,4 @@
+# Assets
+Assets folder is used to store static file that is used. Please group similar file with each other.
+
+Example: images, fonts, etc.
---
+++ src/components/common/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+const Navbar = () => {
+    // Temporary Navbar
+    return (
+        <div className="navbar">
+            This is a temporary navbar.
+        </div>
+    );
+}
+
+export default Navbar;
