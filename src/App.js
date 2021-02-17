4e546626665ae0a7ce5ea9b222f7dbc36d14756f
Ananda Yulizar <anandayulizar@gmail.com>
2021-02-17 20:33:25 +0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from 'components/common/Navbar';
 import Footer from 'components/common/Footer';
+import Login from 'pages/Login';
 import './App.css';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
       <Navbar />
       <div className="page-container">
         {/* Route and PrivateRoute go here */}
-        This is temporary landing page.
+        <Login />
       </div>
       <Footer />
     </div>
--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import './style.css';
+import useWindowSize from 'hooks/useWindowSize';
+import Logo from 'assets/images/logo_jds.png';
 
 const Navbar = () => {
-    // Temporary Navbar
+    const size = useWindowSize();
+
+    // Links bakal tergantung lagi login admin atau tidak, bakal ngikut dari route
+    // Sekarang pake dummy begini dulu
+    const admin = true;
+    // Link-link sementara, bakal bertambah seiring waktu
+    const links = admin ? [
+        'Surveys',
+    ] : [
+            'Home',
+            'Data',
+            'Kontak'
+    ];
     return (
-        <div className="navbar">
-            This is a temporary navbar.
-        </div>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container-fluid">
+                <div className="logo">
+                    <a class="navbar-brand" href="#">
+                        <img src={Logo} alt="" width={80} />
+                    </a>
+                </div>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class={`collapse navbar-collapse ${size.width < 992 ? "" : "nav-align-right"}`} id="navbarNav">
+                    <ul class="navbar-nav">
+                        {links.map(link => {
+                            return (
+                                <li class="nav-item" key={link}>
+                                    <a class="nav-link active" aria-current="page" href="#">{link}</a>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 }
 
