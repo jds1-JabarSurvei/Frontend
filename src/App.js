12c9b8531e2f94b7733403a78146baec99f4efce
Ananda Yulizar <anandayulizar@gmail.com>
2021-02-18 08:24:12 +0700

    add simple route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
+// Components
 import Navbar from 'components/common/Navbar';
 import Footer from 'components/common/Footer';
+// Pages
 import Login from 'pages/Login';
 import Register from 'pages/Register';
+import Index from 'pages/Index';
+// Others
 import './App.css';
+import { Switch, Route, BrowserRouter, HashRouter, Redirect, withRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="page-container">
-        {/* Route and PrivateRoute go here */}
-        <Login />
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="page-container">
+          <Switch>
+            {/* Route and PrivateRoute go here */}
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route path="/" component={Index} />
+          </Switch>
+
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
