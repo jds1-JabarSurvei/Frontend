f944a63472ec06f097c396ed1a84c2b3e3b3086e
Rahmannnnn <arifingatrahman@gmail.com>
2021-02-18 07:30:42 +0700

    perbaikan regex email

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,15 @@
 import { Component } from 'react';
 import './style.css';
 
+const emailValidator = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 class Login extends Component {
     state = {
         isVisible: false,
         email: "",
         password: "",
-        isWrong: false
+        wrongPassword: false,
+        wrongEmail: false
     }
 
     toggleVisiblity = () => {
@@ -14,10 +17,21 @@ class Login extends Component {
         this.setState({ isVisible : !isVisible });
     };
 
-    handleChange = event => { 
-        this.setState({ [event.target.name]: event.target.value });
-        this.setState({ isWrong : false });
+    handlePassword = event => { 
+        this.setState({ password : event.target.value });
+        this.setState({ wrongPassword : false });
     };
+
+    handleEmail = event => {
+        const value = event.target.value;
+        this.setState({ email : value });
+
+        if (!emailValidator.test(value) && value != "") {
+            this.setState({ wrongEmail : true });
+        } else {
+            this.setState({ wrongEmail : false });
+        }
+    }
 
     handleSubmit = event => {
         event.preventDefault();
@@ -26,18 +40,18 @@ class Login extends Component {
             // VALIDASINYA DISINI
             if(email === "aa" && password === "aa"){
                 //Login
-                this.setState({ isWrong : false });
+                this.setState({ wrongPassword : false });
 
             } else {
-                this.setState({ isWrong : true });
+                this.setState({ wrongPassword : true });
             };
         } else {
-            this.setState({ isWrong : false });
+            this.setState({ wrongPassword : false });
         }
     };
 
     render(){
-        const { isVisible, isWrong } = this.state;
+        const { isVisible, wrongPassword, wrongEmail } = this.state;
         return(
             <div className="container">
                 <div className="row content">
@@ -46,18 +60,19 @@ class Login extends Component {
                         <h3 className="signin-text mb-3">Sign In</h3>
                         <form onSubmit={this.handleSubmit}>
                             <div className="form-group">  
-                                <input type="text" name="email" autoComplete="off" onChange={this.handleChange} required />
-                                <label for="email">Email</label>
+                                <input type="text" name="email" autoComplete="off" onChange={this.handleEmail} style={ wrongEmail ? {borderBottom:`2px solid red`} : {}} required />
+                                <label for="email" style={ wrongEmail ? {color: `red`} : {}}>Email</label>
+                                <h6 className="wrong">{ wrongEmail ? "Email must be a valid email" : "" }</h6>
                             </div>
                             <div className="form-group">
-                                <input type={ isVisible ? "text" : "password" } name="password" autoComplete="off" onChange={this.handleChange} required />
-                                <label for="password">Password</label>
+                                <input type={ isVisible ? "text" : "password" } name="password" autoComplete="off" style={ wrongPassword ? {borderBottom:`2px solid red`} : {}} onChange={this.handlePassword} required />
+                                <label for="password" style={ wrongPassword ? {color: `red`} : {}}>Password</label>
                                 <i
                                     className={`fa ${isVisible ? "fa-eye" : "fa-eye-slash"} password-icon`}
-                                    style={ isVisible ? { color:'#198754'} : { color:'#999'}}
+                                    style={ isVisible ? (wrongPassword ? { color:'red'} : { color:'#198754'}) : { color:'#999'}}
                                     onClick={this.toggleVisiblity}
                                 />
-                                <h6 className="wrong">{ isWrong ? "Email/Password yang anda masukkan salah" : "" }</h6>
+                                <h6 className="wrong">{ wrongPassword ? "Incorrect password" : "" }</h6>
                             </div>
                             <input type="submit" name="login" value="Log In" />
                             <h5>Don't have an account? <a href="google.com">Sign Up</a></h5>
