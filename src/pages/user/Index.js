73233807f6a39f2a72d44203c9742be2305704ab
Rahmannnnn <arifingatrahman@gmail.com>
2021-03-15 08:53:02 +0700

    ganti bahasa login

--- src/pages/user/Index.js
+++ src/pages/user/Index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import SurveyList from 'components/user/SurveyList';
 import Carousel from 'components/user/Carousel';
+import Alamat from 'components/user/Alamat';
 
 const Index = () => {
     return (
         <>
             <Carousel/>
             <SurveyList isAdmin={false}/>
+            <Alamat/>
         </>
     );
 }
