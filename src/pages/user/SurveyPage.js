312b873fe500082dc985774219f3bf3ef21679c6
Hizbulloh Ash-Shidiqy <13518047@std.stei.itb.ac.id>
2021-03-22 14:04:26 +0700

    fixing undefined value bug

--- src/pages/user/SurveyPage.js
+++ src/pages/user/SurveyPage.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import NewSurveyContextProvider from 'contexts/NewSurveyContext';
 import './style.css';
 import APICall from "utils/axios"
-import RenderedQuestion from './RenderedQuestion';
+import RenderedQuestion from '../../components/user/RenderedQuestion';
 import { useParams } from 'react-router-dom';
 import Loading from 'components/common/Loading';
 import { toast } from 'react-toastify';
@@ -10,65 +9,12 @@ import { useHistory } from 'react-router-dom';
 
 const SurveyPage = () => {
     const [sectionIdx, setSectionIdx] = useState(0)
-    const [survey, setSurvey] = useState([{ "id_form":"5",
-                                            "pembuat":"user",
-                                            "judulForm":"YUHUHUHU dummy"
-                                            ,"pertanyaan":[
-                                                {"judul":"BAGIAN 1",
-                                                "bagian":0,
-                                                "deskripsi":"YUHUHUHU dummy",
-                                                "pertanyaan":[
-                                                    {"pertanyaan":"YUHUHUHU (dummy)Kenapa Spongebob warnanya kuning?",
-                                                        "id_form_field":10,
-                                                        "deskripsi":"kemukakan jawaban anda mengenai warna kuning dari spongebob",
-                                                        "required":"1",
-                                                        "urutan":0,
-                                                        "tipe":"short_answer",
-                                                        "option":[]},
-                                                    {"pertanyaan":"YUHUHUHU Pertanyaan 2 bagian 0 form 1?",
-                                                        "id_form_field":11,
-                                                        "deskripsi":"deskripsi pertanyaan 2 form 1",
-                                                        "required":"1",
-                                                        "urutan":1,
-                                                        "tipe":"short_answer",
-                                                        "option":[]}
-                                                ]},
-                                                {"judul":"BAGIAN 2",
-                                                "bagian":1,
-                                                "deskripsi":null,
-                                                "pertanyaan":[
-                                                    {"pertanyaan":"YUHUHUHU Pertanyaan 1  bagian 1 form 1?",
-                                                    "id_form_field":12,
-                                                    "deskripsi":"deskripsi pertanyaan 0 bagian 1 form 1",
-                                                    "required":"1",
-                                                    "urutan":0,
-                                                    "tipe":"radio",
-                                                    "option":[
-                                                        {"nilai":"YUHUHUHUa",
-                                                        "id_form_option":10},
-                                                        {"nilai":"YUHUHUHUb",
-                                                        "id_form_option":11},
-                                                        {"nilai":"YUHUHUHUc",
-                                                        "id_form_option":12}]}]},
-                                                {"judul":"BAGIAN 3",
-                                                "bagian":2,
-                                                "deskripsi":null,
-                                                "pertanyaan":[
-                                                    {"pertanyaan":"YUHUHUHU Pertanyaan 1 bagian 2 form 1?",
-                                                    "id_form_field":13,
-                                                    "deskripsi":"deskripsi pertanyaan 1 bagian 2 form 1",
-                                                    "required":"1",
-                                                    "urutan":0,
-                                                    "tipe":"checkbox",
-                                                    "option":[
-                                                        {"nilai":"YUHUHUHUa","id_form_option":13},
-                                                        {"nilai":"YUHUHUHUb","id_form_option":14}
-                                                        ,{"nilai":"YUHUHUHUc","id_form_option":15}]}]}]}]);
+    const [survey, setSurvey] = useState([])
     const history = useHistory();
     let { id } = useParams();
 
     const [answer, setAnswer] = useState([]);
-    // useEffect(() => getQuestionAPI(id), []);
+    useEffect(() => getQuestionAPI(id), []);
 
     const getQuestionAPI = async (id) => {
 
@@ -82,7 +28,6 @@ const SurveyPage = () => {
             }]);
         }).catch(() => {
             setSurvey([]);
-            console.log('ha')
         });
 
     }
@@ -95,14 +40,10 @@ const SurveyPage = () => {
             history.push('/');
             toast.success('Your response has been recorded!');
         }).catch(() =>
-            // alert("GAGAL")
             console.log('halooooo')
         )
     }
-    // useEffect(() => postResponse(), []);
-
-
-    // useEffect(() => console.log(answer[0]), [])
+    
     const back = () => {
         console.log(answer)
         setSectionIdx(sectionIdx - 1);
@@ -197,7 +138,6 @@ const SurveyPage = () => {
                 </>
                 : <Loading />}
         </div>
-
     );
 }
 
