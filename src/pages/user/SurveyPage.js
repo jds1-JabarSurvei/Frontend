ff74faf90d4365bb9fbd90aa2629432843d484bf
Hizbulloh Ash-Shidiqy <13518047@std.stei.itb.ac.id>
2021-03-29 18:49:30 +0700

    Address type question
    Not implemented for post response yet

--- src/pages/user/SurveyPage.js
+++ src/pages/user/SurveyPage.js
@@ -31,14 +31,14 @@ const SurveyPage = () => {
             setSurvey([]);
             console.log('ha')
         });
-
     }
 
     const postResponse = async () => {
         // e.preventDefault();
         await APICall.post("/submitjawaban", {
-            jawaban: answer[0]
+            jawaban: answer
         }).then((res) => {
+            console.log(answer)
             history.push('/');
             toast.success('Your response has been recorded!');
         }).catch(() =>
