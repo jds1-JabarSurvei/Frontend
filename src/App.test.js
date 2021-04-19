37e49fb2e5ab6bd3adca5e79240f1cf0e5e36c5f
rifaldyaristya <rifaldyaristya23@gmail.com>
2021-04-19 15:56:55 +0700

    CICD setup dan simple example

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, cleanup, waitForElement } from '@testing-library/react';
+import Login from './pages/Login';
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Login Page', () => {
+  render(<Login />);
+  //Cek masing-masing field udah ada di page
+  const emailInput = screen.getByTestId('email_input')
+  expect(emailInput).toBeInTheDocument();
+  const emailValidationMessage = screen.getByTestId('email_validation_message')
+  expect(emailValidationMessage).toBeInTheDocument();
+
+  //Cek salah format email
+  userEvent.type(emailInput, 'sasasaa')
+  expect(emailInput).toHaveValue('sasasaa')
+  expect(emailValidationMessage).toHaveTextContent("Email tidak valid")
+
 });
+
+
