568cc7f77e0fe8aa38c0b14ea0419f38a0f96698
Hizbulloh Ash-Shidiqy <13518047@std.stei.itb.ac.id>
2021-04-23 19:51:39 +0700

    add homepage testcase

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen, cleanup, waitForElement } from '@testing-library/react';
 import Login from './pages/Login';
 import userEvent from "@testing-library/user-event";
+import Homepage from './pages/Homepage'
 
 test('Login Page', () => {
   render(<Login />);
@@ -17,4 +18,24 @@ test('Login Page', () => {
 
 });
 
-
+test('Home Page User', () =>{
+  render(<Homepage/>);
+  // carousel
+  const search = screen.getByPlaceholderText("Cari Survei")
+  expect(search).toBeInTheDocument();
+  // survey list
+  const tanggalMenaik = screen.getByText("Tanggal Menaik")
+  expect(tanggalMenaik).toBeInTheDocument();
+  const tanggalMenurun = screen.getByText("Tanggal Menurun")
+  expect(tanggalMenurun).toBeInTheDocument();
+  const abjadMenaik = screen.getByText("Abjad Menaik")
+  expect(abjadMenaik).toBeInTheDocument();
+  const abjadMenurun = screen.getByText("Abjad Menurun")
+  expect(abjadMenurun).toBeInTheDocument();
+  const list = screen.getByLabelText("list")
+  expect(list).toBeInTheDocument();
+  const card = screen.getByLabelText("module")
+  expect(card).toBeInTheDocument();
+  const loading = screen.getByText("Loading...")
+  expect(loading).toBeInTheDocument();
+});
